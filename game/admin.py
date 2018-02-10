36a7c8f311eccad217d86756822fcd4bd1749c68
gauravkulkarni96 <gauravkulkarni96@gmail.com>
2018-02-11 00:15:59 +0530

    admin

--- game/admin.py
+++ game/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import details, scores 
 # Register your models here.
 
-admin.site.register(details)
-admin.site.register(scores)
+class detailAdmin(admin.ModelAdmin):
+	list_display = ('name', 'email', 'phone')
+
+class scoreAdmin(admin.ModelAdmin):
+	list_display = ('user', 'score', 'win', 'date')
+
+admin.site.register(details, detailAdmin)
+admin.site.register(scores, scoreAdmin)
